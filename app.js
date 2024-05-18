910c8397592d75bad9d87808f6c65dfac7d9d3fc
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-05-18 00:49:52 +0000

    8

---
+++ app.js
@@ -0,0 +1,12 @@
+const sum = (a,b) => {
+    return a + b
+}
+console.log(sum(7,3))
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+
+    
+}
